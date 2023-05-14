868efacdbf9b8c04da37e6d1a43c1662fd7431ee
Aditya Butola <butola.aditya10@gmail.com>
2023-05-14 13:45:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,56 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
 
-class Square extends Animal {}
+  get width() {
+    return this._width;
+  }
+
+  set width(value) {
+    if (value <= 0) {
+      throw new Error("Width must be a positive number");
+    }
+    this._width = value;
+  }
+
+  get height() {
+    return this._height;
+  }
+
+  set height(value) {
+    if (value <= 0) {
+      throw new Error("Height must be a positive number");
+    }
+    this._height = value;
+  }
+
+  getArea() {
+    return this.width * this.height;
+  }
+}
+
+class Square extends Rectangle {
+  constructor(side) {
+    super(side, side);
+  }
+
+  get side() {
+    return this.width;
+  }
+
+  set side(value) {
+    this.width = value;
+    this.height = value;
+  }
+
+  getPerimeter() {
+    return 4 * this.width;
+  }
+}
+
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
